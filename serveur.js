ed144bdc1e6c5534b3be1757fce2023fced495b4
Michael <michaelissaly@gmail.com>
2018-06-20 14:15:35 +0400

    affichage des éléments de la bdd dans la page html

--- serveur.js
+++ serveur.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const app = express()
+app.use('/static', express.static('static'));
 
 
 // Route qui pointe vers l'index
-// app.get('/', function (req, res) {
-//     res.sendFile(__dirname + '/index.html')
-// })
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html')
+})
+
 
 var MongoClient = require('mongodb').MongoClient
   , assert = require('assert');
@@ -14,7 +16,7 @@ var MongoClient = require('mongodb').MongoClient
 var url = 'mongodb://localhost:27017/michael';
 
 // Use connect method to connect to the server
-MongoClient.connect(url, function(err, db) {
+MongoClient.connect(url, function (err, db) {
   assert.equal(null, err);
   console.log("Connected successfully to server !!");
 
@@ -22,19 +24,34 @@ MongoClient.connect(url, function(err, db) {
 });
 
 
+app.get('/data', function (req, res) {
+  // res.sendFile(__dirname + '/')
 
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017";
+  var MongoClient = require('mongodb').MongoClient;
+  var url = "mongodb://localhost:27017";
 
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  var dbo = db.db("michael");
-  dbo.collection("personnages").find({}).toArray( function(err, result) {
+  MongoClient.connect(url, function (err, db) {
     if (err) throw err;
-    console.log(result);
-    db.close();
+    var dbo = db.db("michael");
+    dbo.collection("personnages").find({}).toArray(function (err, result) {
+      if (err) throw err;
+      console.log(result);
+      res.send(result);
+      db.close();
+    });
   });
-});
+})
+
+// MongoClient.connect(url, function(err, db) {
+//   if (err) throw err;
+//   var dbo = db.db("michael");
+//   var myobj = { name: "Company Inc", genre: "Highway 37" };
+//   dbo.collection("personnages").insertOne(myobj, function(err, res) {
+//     if (err) throw err;
+//     console.log("1 document inserted");
+//     db.close();
+//   });
+// }); 
 
 
 
